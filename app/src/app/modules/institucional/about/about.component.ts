60e7e841bdcc0feda4393598882470ae67a13052
Raul Victor <raul_souzadias@hotmail.com>
2025-09-30 18:25:10 -0300

    feat: plans page

--- app/src/app/modules/institucional/about/about.component.ts
+++ app/src/app/modules/institucional/about/about.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { fadeInUpBigOnEnterAnimation } from 'angular-animations';
 
 @Component({
   selector: 'app-about',
   imports: [],
   templateUrl: './about.component.html',
+  styleUrl: './about.component.scss',
+  animations: [
+    fadeInUpBigOnEnterAnimation({ translate: '150px', duration: 1500 }),
+  ],
   standalone: true,
 })
 export class AboutComponent {}
--- app/src/app/modules/institucional/plans/plans.component.ts
+++ app/src/app/modules/institucional/plans/plans.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { fadeInDownBigOnEnterAnimation } from 'angular-animations';
 
 @Component({
   selector: 'app-plans',
   imports: [],
   templateUrl: './plans.component.html',
+  styleUrl: './plans.component.scss',
+  animations: [fadeInDownBigOnEnterAnimation({ translate: '150px' })],
   standalone: true,
 })
 export class PlansComponent {}
