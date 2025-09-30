fdfd6cec31f4edc3d3f54b66715f7772d6ccc59b
Raul Victor <raul_souzadias@hotmail.com>
2025-09-30 17:14:56 -0300

    feat: add animations

--- app/src/app/modules/institucional/home/home.component.ts
+++ app/src/app/modules/institucional/home/home.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { RouterLink } from '@angular/router';
+import {
+  fadeInRightBigOnEnterAnimation,
+  fadeInLeftBigOnEnterAnimation,
+} from 'angular-animations';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatIconModule,
+  ],
+  animations: [
+    fadeInRightBigOnEnterAnimation({ translate: '250px', duration: 1500 }),
+    fadeInLeftBigOnEnterAnimation({ translate: '250px', duration: 1500 }),
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true,
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  form!: FormGroup;
+  constructor(private _formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this._formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      company: ['', Validators.required],
+      phone: ['', Validators.required],
+    });
+  }
+}
+function faceInRightOnEnter(arg0: {
+  translate: string;
+  duration: number;
+}): any {
+  throw new Error('Function not implemented.');
+}
