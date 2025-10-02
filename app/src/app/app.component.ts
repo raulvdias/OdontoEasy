d1a37ba1b1464ef083c4bba41acd050b366376f9
Raul Victor <raul_souzadias@hotmail.com>
2025-10-02 11:08:32 -0300

    feat: mobile navbar

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,5 +8,40 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'app';
+  title = 'OdontoEasy';
+  screenSize = window.innerWidth > 1000;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenSize = event.target.innerWidth > 1000;
+
+    let bodyHasNoScroll = false;
+    let htmlHasNoScroll = false;
+
+    if (this.screenSize) {
+      document.querySelectorAll('body').forEach((body) => {
+        if (body.classList.contains('no-scroll')) {
+          bodyHasNoScroll = true;
+        }
+      });
+
+      document.querySelectorAll('html').forEach((html) => {
+        if (html.classList.contains('no-scroll')) {
+          htmlHasNoScroll = true;
+        }
+      });
+    }
+
+    if (
+      (this.screenSize && bodyHasNoScroll) ||
+      (this.screenSize && htmlHasNoScroll)
+    ) {
+      document.querySelectorAll('body').forEach((body) => {
+        body.classList.remove('no-scroll');
+      });
+
+      document.querySelectorAll('html').forEach((html) => {
+        html.classList.remove('no-scroll');
+      });
+    }
+  }
 }
--- app/src/app/common/institucional/nav/nav.component.ts
+++ app/src/app/common/institucional/nav/nav.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [RouterLink, MatButtonModule],
+  imports: [RouterLink, MatButtonModule, NgIf, MatIconModule],
   templateUrl: './nav.component.html',
   standalone: true,
 })
-export class NavComponent {}
+export class NavComponent implements OnInit {
+  screenSize: boolean = window.innerWidth > 1000;
+  isOpened = false;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenSize = event.target.innerWidth > 1000;
+
+    if (this.screenSize) {
+      this.isOpened = false;
+    }
+  }
+
+  ngOnInit(): void {}
+
+  openClose() {
+    this.isOpened = !this.isOpened;
+
+    document.querySelectorAll('body').forEach((body) => {
+      body.classList.toggle('no-scroll');
+    });
+
+    document.querySelectorAll('html').forEach((html) => {
+      html.classList.toggle('no-scroll');
+    });
+  }
+}
