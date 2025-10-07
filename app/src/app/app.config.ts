ceb94276fd3193706b4fee4900377181adb0b7e4
Raul Victor <raul_souzadias@hotmail.com>
2025-10-07 16:39:11 -0300

    feat: firebase hosting

--- app/src/app/app.config.ts
+++ app/src/app/app.config.ts
@@ -1,12 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { routes } from './app.routes';
+import { environment } from './environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
   ],
 };
---
+++ app/src/app/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAYdRHd3G1i9vvdyJ3CV2pghGPYO8OkEO8',
+    authDomain: 'odontoeasy-c0202.firebaseapp.com',
+    projectId: 'odontoeasy-c0202',
+    storageBucket: 'odontoeasy-c0202.firebasestorage.app',
+    messagingSenderId: '1012570993411',
+    appId: '1:1012570993411:web:8618450dd19e7abeb8af36',
+    measurementId: 'G-EWDT4501YV',
+  },
+  api: 'http://localhost:3333',
+  version: '0.0.1',
+  mode: 'common',
+};
